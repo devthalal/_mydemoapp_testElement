47c87abad5b64b31f489d977bae7246cccf97fb8
devthalal <thalal.hassan@neoito.com>
2022-04-04 17:17:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Input from './testElement'
+export default function App() {
+  return (
+    <div>
+      <testElement/>
+    </div>
+  )
+}
---
+++ src/testElement.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const testElement = () => {
+  return (
+   <p>Hello from testElement </p>
+  )
+}
+export default testElement
